601f2ef3dd7a414c257faabb05535e58316ad6d2
Elena Zarkova <ezarkova@gmail.com>
2017-03-10 15:34:50 +0200

    Project skeleton

---
+++ CourseProject/CourseProject.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourseProject.Web.Startup))]
+namespace CourseProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
