e34691e88dfb71d1200778c720f592811a0a4a09
Elena Zarkova <ezarkova@gmail.com>
2017-03-12 20:51:49 +0200

    Admin partial

--- CourseProject/CourseProject.Web/Controllers/AccountController.cs
+++ CourseProject/CourseProject.Web/Controllers/AccountController.cs
@@ -154,9 +154,11 @@ namespace CourseProject.Web.Controllers
             if (ModelState.IsValid)
             {
                 var user = new User { UserName = model.Username, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var result = await this.UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    this.UserManager.AddToRole(user.Id, RoleNames.Regular);
+
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
