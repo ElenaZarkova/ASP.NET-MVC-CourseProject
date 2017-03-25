1197701423bbb993db790783ac2c4ac245fe1bf7
Elena Zarkova <ezarkova@gmail.com>
2017-03-25 16:48:02 +0200

    More books service tests

--- CourseProject/CourseProject.Services.Tests/BooksServiceTests/AddBook_Should.cs
+++ CourseProject/CourseProject.Services.Tests/BooksServiceTests/AddBook_Should.cs
@@ -4,58 +4,88 @@ using NUnit.Framework;
 using CourseProject.Services;
 using CourseProject.Data.Contracts;
 using CourseProject.Models;
+using CourseProject.ViewModels.Admin.AddBook;
 
 namespace CourseProject.Services.Tests.BooksServiceTests
 {
     [TestFixture]
     public class AddBook_Should
     {
+        [Test]
+        public void CallDataBooksAddMethod()
+        {
+            // Arrange
+            var bookModel = new BookModel();
+            var mockedData = new Mock<IBetterReadsData>();
+            mockedData.Setup(x => x.Books.Add(It.IsAny<Book>())).Verifiable();
+
+            var service = new BooksService(mockedData.Object);
+
+            // Act
+            service.AddBook(bookModel, "filename");
+
+            // Assert
+            mockedData.Verify(x => x.Books.Add(It.IsAny<Book>()), Times.Once);
+        }
+
+        [Test]
+        public void CallDataBooksAddMethodWithBookWithCorrectProperties()
+        {
+            // Arrange
+            var bookModel = new BookModel()
+            {
+                Title = "Title",
+                Author = "TheAuthor",
+                Description = "Some description",
+                GenreId = 5,
+                PublishedOn = new DateTime(2014, 3, 5)
+            };
+            var filename = "pic.jpg";
+            var mockedData = new Mock<IBetterReadsData>();
+            Book addedBook = null;
+            mockedData.Setup(x => x.Books.Add(It.IsAny<Book>()))
+                .Callback((Book book) => addedBook = book);
+
+            var service = new BooksService(mockedData.Object);
+
+            // Act
+            service.AddBook(bookModel, filename);
+
+            // Assert
+            Assert.AreEqual(bookModel.Title, addedBook.Title);
+            Assert.AreEqual(bookModel.Author, addedBook.Author);
+            Assert.AreEqual(bookModel.Description, addedBook.Description);
+            Assert.AreEqual(bookModel.GenreId, addedBook.GenreId);
+            Assert.AreEqual(bookModel.PublishedOn, addedBook.PublishedOn);
+            Assert.AreEqual(filename, addedBook.CoverFile);
+        }
+
+        [Test]
+        public void CallDataSaveChanges()
+        {
+            // Arrange
+            var mockedData = new Mock<IBetterReadsData>();
+            mockedData.Setup(x => x.Books.Add(It.IsAny<Book>())).Verifiable();
+            mockedData.Setup(x => x.SaveChanges()).Verifiable();
+            var service = new BooksService(mockedData.Object);
+
+            // Act
+            service.AddBook(new BookModel(), "filename");
+
+            // Assert
+            mockedData.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
         //[Test]
-        //public void CallDataBooksAddMethod()
+        //public void ReturnCorrectBookId()
         //{
         //    // Arrange
-        //    var mockedBook = new Mock<Book>();
+        //    var bookModel = new BookModel();
         //    var mockedData = new Mock<IBetterReadsData>();
-        //    mockedData.Setup(x => x.Books.Add(It.IsAny<Book>())).Verifiable();
-
         //    var service = new BooksService(mockedData.Object);
 
         //    // Act
-        //    service.AddBook(mockedBook.Object);
-
-        //    // Assert
-        //    mockedData.Verify(x => x.Books.Add(It.IsAny<Book>()), Times.Once);
-        //}
-
-        //[Test]
-        //public void CallDataBooksAddMethodWithCorrectBook()
-        //{
-        //    // Arrange
-        //    var mockedBook = new Mock<Book>();
-        //    var mockedData = new Mock<IBetterReadsData>();
-        //    mockedData.Setup(x => x.Books.Add(mockedBook.Object)).Verifiable();
-
-        //    var service = new BooksService(mockedData.Object);
-
-        //    // Act
-        //    service.AddBook(mockedBook.Object);
-
-        //    // Assert
-        //    mockedData.Verify(x => x.Books.Add(mockedBook.Object), Times.Once);
-        //}
-
-        //[Test]
-        //public void CallDataSaveChanges()
-        //{
-        //    // Arrange
-        //    var mockedBook = new Mock<Book>();
-        //    var mockedData = new Mock<IBetterReadsData>();
-        //    mockedData.Setup(x => x.Books.Add(It.IsAny<Book>())).Verifiable();
-        //    mockedData.Setup(x => x.SaveChanges()).Verifiable();
-        //    var service = new BooksService(mockedData.Object);
-
-        //    // Act
-        //    service.AddBook(mockedBook.Object);
+        //    service.AddBook(new BookModel(), "filename");
 
         //    // Assert
         //    mockedData.Verify(x => x.SaveChanges(), Times.Once);
---
+++ CourseProject/CourseProject.Services.Tests/BooksServiceTests/BookWithTitleExists_Should.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using CourseProject.Data.Contracts;
+using CourseProject.Models;
+
+namespace CourseProject.Services.Tests.BooksServiceTests
+{
+    [TestFixture]
+    public class BookWithTitleExists_Should
+    {
+        [Test]
+        public void ReturnTrueWhenBookFound()
+        {
+            // Arrange
+            var title = "Title";
+            var mockedData = new Mock<IBetterReadsData>();
+            var matchedBook = new Book() { Title = title };
+            var books = new List<Book>
+            {
+                 new Mock<Book>().Object,
+                 new Mock<Book>().Object,
+                 matchedBook
+            }.AsQueryable();
+
+            mockedData.Setup(x => x.Books.All).Returns(books);
+
+            var service = new BooksService(mockedData.Object);
+
+            // Act
+            var exists = service.BookWithTitleExists(title);
+
+            // Assert
+            Assert.IsTrue(exists);
+        }
+
+        [Test]
+        public void ReturnFalseWhenBookNotFound()
+        {
+            // Arrange
+            var mockedData = new Mock<IBetterReadsData>();
+            var books = new List<Book>
+            {
+                 new Mock<Book>().Object,
+                 new Mock<Book>().Object,
+                 new Mock<Book>().Object
+            }.AsQueryable();
+
+            mockedData.Setup(x => x.Books.All).Returns(books);
+
+            var service = new BooksService(mockedData.Object);
+
+            // Act
+            var exists = service.BookWithTitleExists("Title");
+
+            // Assert
+            Assert.IsFalse(exists);
+        }
+    }
+}
